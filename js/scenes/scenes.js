3754ebdbf7c0d1e5a5d1379d7c9537d88c7a8635
Daria <dariaxu1997@outlook.com>
2023-05-11 15:42:16 -0400

    before merge

--- js/scenes/scenes.js
+++ js/scenes/scenes.js
@@ -31,7 +31,7 @@ export default () => {
          // { name: "DemoSprite"         , path: "./demoSprite.js"         },
          // { name: "DemoIntersect"      , path: "./demoIntersect.js"      },
          { name: "DemoCroquet"        , path: "./demoCroquet.js"      },
-         { name: "seaPrimitive"  , path: "./seaPrimitive.js"  },
+         { name: "seaPrimitive"  , path: "./seaPrimitive2.js"  },
          { name: "seaCroquet"  , path: "./seaCroquet.js"  },
       ]
    };
